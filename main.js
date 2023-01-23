a7a4b092b602fdf6f3bfd222021d1d5b2841e125
stephanie <1919731@chester.ac.uk>
2023-01-23 09:28:46 +0000

    fix format

--- main.js
+++ main.js
@@ -3,30 +3,32 @@ const url = `https://api.thecatapi.com/v1/images/search?limit=12`;
 const api_key = "live_qtoWFwBFjh3eaGRU0Vw49yQH13EHWpql5P2juMbgbthEutYOiXzu0OeYWc5xDxSK";
 
 
-fetch(url,{headers: {
-  'x-api-key': api_key
-}})
-.then((response) => {
-return response.json();
+fetch(url, {
+  headers: {
+    'x-api-key': api_key
+  }
 })
-.then((data) => {
-let imagesData = data;
-imagesData.map(function(imageData) {
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    let imagesData = data;
+    imagesData.map(function (imageData) {
 
-let image = document.createElement('img');
-//use the url from the image object
-image.src = `${imageData.url}`;
-    
-let gridCell = document.createElement('div');
-gridCell.classList.add('clmn');
-gridCell.classList.add('clmnShadow');
-gridCell.appendChild(image)
-  
-document.getElementById('grid').appendChild(gridCell);
+      let image = document.createElement('img');
+      //use the url from the image object
+      image.src = `${imageData.url}`;
 
-});
-})
-.catch(function(error) {
-console.log(error);
-});
+      let gridCell = document.createElement('div');
+      gridCell.classList.add('clmn');
+      gridCell.classList.add('clmnShadow');
+      gridCell.appendChild(image)
+
+      document.getElementById('grid').appendChild(gridCell);
+
+    });
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
